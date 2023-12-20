38aef91ec6ff8f9b4eba133602408e099a75d44a
Konstantin Baranov <irongrol@gmail.com>
2023-12-20 07:56:09 +0300

    add method for expand Game Description

--- src/main/java/pages/GamePage.java
+++ src/main/java/pages/GamePage.java
@@ -1,8 +1,6 @@
 package pages;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import com.google.common.collect.ImmutableList;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import pages.blocks.DlcList;
@@ -15,10 +13,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class GamePage extends BasePage {
+
+    public static final String COLLAPSED_SECTION_CLASS_NAME = ".game_page_autocollapse_ctn collapsed";
     public static final By GAME_TITLE_LOCATOR = By.cssSelector(".apphub_HomeHeaderContent");
     public static final By GAME_DESCRIPTION_LOCATOR = By.cssSelector(".game_area_description");
     public static final By GAME_REQUIREMENTS_LOCATOR = By.cssSelector(".sysreq_contents");
     public static final By GAME_SCREENSHOTS_LOCATOR = By.cssSelector(".highlight_screenshot_link");
+    public static final By READ_MORE_LOCATOR = By.cssSelector(".game_page_autocollapse_readmore");
 
     private DlcList dlcList;
 
@@ -46,6 +47,14 @@ public class GamePage extends BasePage {
 
         Assert.assertTrue(actualUrls.containsAll(expectedUrls) && expectedUrls.containsAll(actualUrls),
                 "Actual screenshots doesn't match with expected");
+        return this;
+    }
+
+    public GamePage expandDescription(){
+        $(GAME_DESCRIPTION_LOCATOR)
+                .ancestor(COLLAPSED_SECTION_CLASS_NAME)
+                .find(READ_MORE_LOCATOR)
+                .click();
         return this;
     }
 
