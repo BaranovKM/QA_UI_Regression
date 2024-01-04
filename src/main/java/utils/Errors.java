18eb5e675b7ad872cbbc58a0d8535ba08d779a8e
Konstantin Baranov <irongrol@gmail.com>
2024-01-04 20:04:46 +0300

    add class with errors

---
+++ src/main/java/utils/Errors.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Errors {
+    public static final String ATTRIBUTE_MISMATCH_ERROR = "Attribute value doesn't match with expected. \n Actual value : %s \n Expected value : %s \n";
+    public static final String URLS_MISMATCH_ERROR = "URLs doesn't match with expected.";
+    public static final String IMAGES_MISMATCH_ERROR = "Images are different";
+    public static final String PAGE_TITLE_MISMATCH_ERROR = "Page doesn't have expected title";
+    public static final String PAGE_HEADER_ERROR = "Something wrong with page header";
+    public static final String PAGE_FOOTER_ERROR = "Something wrong with page header";
+}
